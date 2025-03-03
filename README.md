296977e3dfc69775ae684b30acd16a8f67ff42d9
Ian Bentley <ian@idbentley.com>
2025-03-03 15:32:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Clerk Cake Example
+
+Install dependencies:
+
+```bash
+$ composer update
+```
+
+Make sure the CLERK_SECRET_KEY [environment variable](https://clerk.com/docs/deployments/clerk-environment-variables#clerk-publishable-and-secret-keys) is set, ie:
+
+```bash
+$ export CLERK_SECRET_KEY=my_secret_key
+```
+
+Start the server:
+
+```bash
+$ bin/cake server
+```
+
+Set `clerk.authorized_parties` in config/app_local.php:
+```php
+    'Clerk' => [
+        'secret_key' => env("SECRET_KEY"),
+        'authorized_parties' => ['http://localhost:5173'] # default location for clerk react app
+    ]
+```
+
+From a Clerk frontend, use the `useSession` hook to retrieve the getToken() function:
+
+```js
+const session = useSession();
+const getToken = session?.session?.getToken
+```
+
+Then, request the python server:
+
+```js
+if (getToken) {
+    // get the userId or null if the token is invalid
+    let res = await fetch("http://localhost:8765/Protected/clerk_jwt", {
+        headers: {
+            "Authorization": `Bearer ${await getToken()}`
+        }
+    })
+    console.log(await res.json()) // {userId: 'the_user_id_or_null'}
+
+    // get gated data or a 401 Unauthorized if the token is not valid
+    res = await fetch("http://localhost:8765/Protected/get_gated", {
+        headers: {
+            "Authorization": `Bearer ${await getToken()}`
+        }
+    })
+    if (res.ok) {
+        console.log(await res.json()) // {foo: "bar"}
+
+    } else {
+        // token was invalid
+    }
+}
+```
---
+++ src/Controller/ProtectedController.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link      https://cakephp.org CakePHP(tm) Project
+ * @since     0.2.9
+ * @license   https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\Http\Response;
+use Cake\View\Exception\MissingTemplateException;
+
+/**
+ * Static content controller
+ *
+ * This controller will render views from templates/Pages/
+ *
+ * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
+ */
+class ProtectedController extends AppController
+{
+    public function viewClasses(): array
+    {
+        return [JsonView::class];
+    }
+
+    public function clerkJwt()
+    {
+        $this->viewBuilder()->setClassName('Json');
+
+        $authPayload = $this->getRequest()->getAttribute('verified_clerk_payload');
+        if ($authPayload !== null) {
+            $this->set('data', ['userId' => $authPayload->sub]);
+        } else {
+            $this->set('data', ['userId' => null]);
+         }
+        $this->viewBuilder()->setOption('serialize', ['data']);
+
+    }
+
+    public function getGated()
+    {
+        $this->viewBuilder()->setClassName('Json');
+
+        $authPayload = $this->getRequest()->getAttribute('verified_clerk_payload');
+        if ($authPayload != null && $authPayload->sub) {
+            $this->set('data', ['foo' => 'bar']);
+        } else {
+            throw new ForbiddenException('You are not authorized to access this resource');
+        }
+
+        $this->viewBuilder()->setOption('serialize', ['data']);
+    }
+
+}
